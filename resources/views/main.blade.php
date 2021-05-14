81bd6f80d7cd7548acc425df0506c41600e98f4b
TheF4stB0i <neelissimo@gmail.com>
2021-05-14 14:25:56 +0200

    Add Logo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
         <title>Pok&eacute;Cards @yield('title')</title>
     </head>
     <body>
-        <h1>Pok&eacute;Cards</h1>
+        <h1><img src="https://fontmeme.com/permalink/210514/7380c713c6ccc27634273d42b7c170f1.png" alt="pokemon-schriftart" border="0"></h1>
         <a href="{{ route('main') }}"><button>Main</button></a>
         <a href="{{ route('trading') }}"><button>Trading</button></a>
         <a href="{{ route('wiki') }}"><button>Wiki</button></a>
