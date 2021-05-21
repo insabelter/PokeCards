853c18dee15e32a2bbf62797c5a0744de6276aae
Insa Belter <insa.belter@gmx.de>
2021-05-21 12:19:58 +0200

    configured edit buttons

--- resources/views/pages/account/profile.blade.php
+++ resources/views/pages/account/profile.blade.php
@@ -5,25 +5,42 @@
 @section('content')
     <h1>Profile</h1>
 
-    <button type="button" class="btn btn-primary">Edit Information</button>
+    <button type="button" id="startEditing" class="btn btn-primary" onclick="startEditing()">Edit Information</button>
     <form method="post" action="/profile">
         @csrf
         <div class="form-group">
             <label for="usernameID">Username:</label>
             <input type="text" class="form-control" id="usernameID" name="username" value="User" readonly>
         </div>
-        <div class="form-group">
-            <label for="passwordID">Password:</label>
-            <input type="password" class="form-control" id="passwordID" name="password" value="Password" readonly>
-        </div>
-        <div class="form-group">
-            <label for="emailID">E-Mail:</label>
-            <input type="email" class="form-control" id="emailID" name="email" value="test@mail.com" readonly>
-        </div>
-        <div class="form-group">
-            <label for="telefonID">Telefon:</label>
-            <input type="text" class="form-control" id="telefonID" name="telefon" value="+01 23456789" readonly>
-        </div>
-        <button type="submit" class="btn btn-primary" disabled>Update Information</button>
+        <fieldset id="editableFieldset" disabled>
+            <div class="form-group">
+                <label for="passwordID">Password:</label>
+                <input type="password" class="form-control" id="passwordID" name="password" value="Password">
+            </div>
+            <div class="form-group">
+                <label for="emailID">E-Mail:</label>
+                <input type="email" class="form-control" id="emailID" name="email" value="test@mail.com">
+            </div>
+            <div class="form-group">
+                <label for="telefonID">Telefon:</label>
+                <input type="text" class="form-control" id="telefonID" name="telefon" value="+01 23456789">
+            </div>
+            <button type="submit" class="btn btn-primary" onclick="">Update Information</button>
+        </fieldset>
     </form>
+
+    <script>
+        let editing = false;
+        let fieldset= document.getElementById("editableFieldset");
+        let startEdit= document.getElementById("startEditing");
+        console.log(fieldset)
+        function startEditing(){
+            editing = true;
+            fieldset.disabled = false;
+            startEdit.disabled = true;
+        }
+    </script>
 @endsection
+
+
+
