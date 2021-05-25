83f2c465405a168999d5e9ec647aff8c1eaa4154
TheF4stB0i <neelissimo@gmail.com>
2021-05-25 17:10:06 +0200

    Implement actual database access

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cards;
+use App\Models\Sets;
 use Illuminate\Http\Request;
 
 class WikiController extends Controller
@@ -9,23 +11,11 @@ class WikiController extends Controller
     public function index(){
 //        TODO: Get DB Data
 //        Example Data:
-        $cardArray = [];
-        $card_arr1=array("id"=>"1","set"=>"Base", "name"=>"Alakazam", "type"=>"Pokémon", "release"=>"1999/01/09", "image"=>"https://images.pokemontcg.io/base1/1_hires.png");
-        $card1=(object)$card_arr1;
-        $cardArray[] = $card1;
-        $card_arr2=array("id"=>"2","set"=>"Jungle", "name"=>"Bellsprout", "type"=>"Pokémon", "release"=>"1999/06/16", "image"=>"https://images.pokemontcg.io/base2/49_hires.png");
-        $card2=(object)$card_arr2;
-        $cardArray[] = $card2;
-        $card_arr3=array("id"=>"3","set"=>"Fossil", "name"=>"Hypno", "type"=>"Pokémon", "release"=>"1999/10/10", "image"=>"https://images.pokemontcg.io/base3/23_hires.png");
-        $card3=(object)$card_arr3;
-        $cardArray[] = $card3;
-        $card_arr4=array("id"=>"4","set"=>"Fossil", "name"=>"Mr. Fuji", "type"=>"Trainer", "release"=>"1999/10/10", "image"=>"https://images.pokemontcg.io/base3/58_hires.png");
-        $card4=(object)$card_arr4;
-        $cardArray[] = $card4;
-        $card_arr5=array("id"=>"5","set"=>"Black & White", "name"=>"Sawk", "type"=>"Pokémon", "release"=>"2011/04/25", "image"=>"https://images.pokemontcg.io/bw1/62_hires.png");
-        $card5=(object)$card_arr5;
-        $cardArray[] = $card5;
+        $cards = Cards::all();
+        return view('pages.wiki', compact('cards'));
+    }
 
-        return view('pages.wiki', compact('cardArray'));
+    public function getSetName($setId){
+        return Sets::find($setId)->name;
     }
 }
--- resources/views/pages/wiki.blade.php
+++ resources/views/pages/wiki.blade.php
@@ -14,17 +14,15 @@
             <th scope="col">Set</th>
             <th scope="col">Name</th>
             <th scope="col">Card Type</th>
-            <th scope="col">Release Date</th>
             <th scope="col">Image</th>
         </tr>
         </thead>
         <tbody>
-        @foreach($cardArray as $card)
+        @foreach($cards as $card)
             <tr>
-                <td>{{ $card->set }}</td>
+                <td>{{ $card->setId }}</td>
                 <td>{{ $card->name }}</td>
-                <td>{{ $card->type }}</td>
-                <td>{{ $card->release }}</td>
+                <td>{{ $card->cardtype }}</td>
                 <td id="accordion{{$card->id}}">
                     <button class="btn btn-primary" data-toggle="collapse" data-target="#collapse{{$card->id}}" aria-expanded="false" aria-controls="collapse{{$card->id}}" onclick="toggleText(this)">
                         Show
@@ -33,7 +31,7 @@
             </tr>
             <tr>
                 <td colspan="5" id="collapse{{$card->id}}" class="collapse" data-parent="#accordion{{$card->id}}" style="text-align: center;">
-                    <img src="{{ $card->image }}" alt="Image {{ $card->name }}" height="500px">
+                    <img src="{{ $card->smallImage }}" alt="Image {{ $card->name }}" height="500px">
                 </td>
             </tr>
         @endforeach
