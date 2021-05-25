d9f1a94dcd0363be4f399166001a3bdb7c3d54c7
TheF4stB0i <neelissimo@gmail.com>
2021-05-25 15:46:20 +0200

    Added Models for new Tables

---
+++ app/Models/Sets.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sets extends Model
+{
+    protected $fillable=[
+        'name',
+        'seriesName',
+        'printedTotal',
+        'releaseDate'
+    ];
+}
---
+++ database/migrations/2021_05_25_133919_create_prices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id('cardId');
+            $table->float('normal');
+            $table->float('holofoil');
+            $table->float('reverseHolofoil');
+            $table->float('firstEditionHolofoil');
+            $table->string('buyURL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
