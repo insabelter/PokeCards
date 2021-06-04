c4fa63b86b9c800c4ed27473cccde52ccd1cf639
Insa Belter <insa.belter@gmx.de>
2021-06-04 15:45:41 +0200

    added custom reset password mail

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\URL;
+
+class ResetPasswordNotification extends ResetPassword
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public $token;
+    public function __construct($data)
+    {
+        $this->token = $data;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = url(URL::to('/') . route('password.reset', [
+                'token' => $this->token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ], false));
+
+        return (new MailMessage)->view('emails.reset-password', ['url' => $url]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -1,9 +1,7 @@
 <html>
     <div style="background-color: var(--primary-light)">
         <img src="https://fontmeme.com/permalink/210514/7380c713c6ccc27634273d42b7c170f1.png" alt="pokemon-schriftart" border="0" height="60"> <br>
-        <h1>Please Verify your E-Mail-Adress!</h1>
-        <a href="{{ $url }}">Verify</a>
+        <h1>Reset your Password:</h1>
+        <a href="{{ $url }}">Reset Password</a>
     </div>
 </html>
-
-
