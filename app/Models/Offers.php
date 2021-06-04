9d44cd8ebc315176b6b99e476f80374c4b9f8350
TheF4stB0i <neelissimo@gmail.com>
2021-06-04 12:45:47 +0200

    Add Offer model

---
+++ app/Models/Offers.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offers extends Model
+{
+    protected $table = 'Offers';
+    protected $keyType = 'int';
+    public $timestamps = false;
+    protected $primaryKey='offerId';
+    protected $fillable=[
+        'cardId',
+        'description',
+        'userId'
+    ];
+
+}
