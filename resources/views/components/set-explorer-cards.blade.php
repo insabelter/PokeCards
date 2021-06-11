ed9e2a0578bc35c5fce4216aca5cd8454030c6fd
Insa Belter <insa.belter@gmx.de>
2021-06-11 18:10:06 +0200

    modals finished

--- resources/views/components/set-explorer-cards.blade.php
+++ resources/views/components/set-explorer-cards.blade.php
@@ -1,27 +1,25 @@
-<div class="row">
-    @foreach($cardArray as $card)
-        <div class="col-lg-3 col-md-4 col-6" style="padding: 0;">
-            <div class="card" style="margin: 10px;">
-{{--                Modal Button --}}
-                <input type="image" data-toggle="modal" data-target="#cardModal{{$card->id}}" src="{{$card->smallImage}}" alt="{{$card->name}}"/>
-                {{-- Modal for Card --}}
-                <div class="modal fade" id="cardModal{{$card->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <img src="{{$card->largeImage}}" alt="{{$card->name}}">
-                            <span>
-                                <button class="btn btn-primary btn-small" style="margin: 10px;">
-                                    <a href="{{ route('marketplace') }}">Search on Marketplace</a>
-                                </button>
-                                <button class="btn btn-primary btn-small" style="margin: 10px; float: right;">
-                                    <a href="{{ route('offers') }}">Create Offer</a>
-                                </button>
+<div class="container">
+    <div class="row">
+        @foreach($cardArray as $card)
+            <div class="col-lg-3 col-md-4 col-6" style="padding: 10px;">
+                <div class="card" style="margin: 0;">
+                    {{--                Modal Button --}}
+                    <input type="image" data-toggle="modal" data-target="#cardModal{{$card->id}}" src="{{$card->smallImage}}" alt="{{$card->name}}"/>
+                    {{-- Modal for Card --}}
+                    <div class="modal fade" id="cardModal{{$card->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <img style="padding: 10px;" src="{{$card->largeImage}}" alt="{{$card->name}}">
+                                <span>
+                                <a class="btn btn-primary btn-small" style="margin: 0 10px 10px 10px;" href="{{ route('marketplace') }}">Search on Marketplace</a>
+                                <a class="btn btn-primary btn-small" style="margin: 0 10px 10px 10px; float: right;" href="{{ route('offers') }}">Create Offer</a>
                             </span>
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
+
