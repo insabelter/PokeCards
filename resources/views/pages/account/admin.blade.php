41c8785fd83117adcb529f53b1c28038b12b69f6
Noah <nowag4000@web.de>
2021-06-30 13:20:44 +0200

    Admin page

--- resources/views/pages/account/admin.blade.php
+++ resources/views/pages/account/admin.blade.php
@@ -20,9 +20,12 @@
     <table class="table table-hover" style="">
         <thead>
         <tr class="table-primary">
-            <th scope="col" style="width: 25%;">Name</th>
-            <th scope="col" style="width: 25%;">E-Mail</th>
-            <th scope="col" style="width: 25%;">Verified</th>
+            <th scope="col" style="width: 20%;">Name</th>
+            <th scope="col" style="width: 20%;">E-Mail</th>
+            <th scope="col" style="width: 20%;">Verified</th>
+            <th scope="col" style="width: 10%;">Is Admin?</th>
+            <th scope="col" style="width: 15%;">Make Admin</th>
+            <th scope="col" style="width: 15%;">Delete</th>
         </tr>
         </thead>
         <tbody>
@@ -36,8 +39,35 @@
                         <td>{{ $user->email }}
                         </td>
                         <td>{{ $user->email_verified_at }}</td>
+                        @if($user->is_admin == 1)
+                            <td>yes</td>
+                        @else
+                            <td>no</td>
+                        @endif
+                        <td class="d-none d-sm-table-cell">
+                            <form action="">
+                                @csrf
+                                <a class="btn btn-primary" href="">Make Admin</a>
+                            </form>
+                        </td>
+                        <td class="d-none d-sm-table-cell">
+                            <form action="">
+                                @csrf
+                                <a class="btn btn-primary" href="">Delete</a>
+                            </form>
+                        </td>
                     </tr>
                 @endif
+            @endforeach
+        @else
+            @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->name }} <br>
+                        </td>
+                        <td>{{ $user->email }}
+                        </td>
+                        <td>{{ $user->email_verified_at }}</td>
+                    </tr>
             @endforeach
         @endif
         </tbody>
