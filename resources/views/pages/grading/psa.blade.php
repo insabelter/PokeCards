c162c3386930ef103f52b9141c71c02612232eab
Noah <nowag4000@web.de>
2021-06-30 21:24:55 +0200

    psa calculator ready

--- resources/views/pages/grading/psa.blade.php
+++ resources/views/pages/grading/psa.blade.php
@@ -15,22 +15,27 @@
     </div>
     <div class="form-group">
         <label for="regularcard">cards valued between 500$ and 999$:</label>
-        <input type="number" class="form-control" id="regularcard" name="test" min="0" oninput="validity.valid||(value='');" placeholder="insert number of cards">
+        <input type="number" class="form-control" id="regularcard" name="regular" min="0" oninput="validity.valid||(value='');" placeholder="insert number of cards">
     </div>
     <div class="form-group">
         <label for="expresscard">cards valued between 1000$ and 2499$:</label>
-        <input type="number" class="form-control" id="expresscard" name="test" min="0" oninput="validity.valid||(value='');" placeholder="insert number of cards">
+        <input type="number" class="form-control" id="expresscard" name="express" min="0" oninput="validity.valid||(value='');" placeholder="insert number of cards">
     </div>
     <div class="form-group">
         <label for="superexpresscard">cards valued between 2500$ and 4999$:</label>
-        <input type="number" class="form-control" id="superexpresscard" name="test" min="0" oninput="validity.valid||(value='');" placeholder="insert number of cards">
+        <input type="number" class="form-control" id="superexpresscard" name="superexpress" min="0" oninput="validity.valid||(value='');" placeholder="insert number of cards">
     </div>
     <div class="form-group">
         <label for="walk-throughcard">cards valued between 5000$ and 9999$:</label>
-        <input type="number" class="form-control" id="walk-throughcard" name="test" min="0" oninput="validity.valid||(value='');" placeholder="insert number of cards">
+        <input type="number" class="form-control" id="walkthroughcard" name="walk-through" min="0" oninput="validity.valid||(value='');" placeholder="insert number of cards">
     </div>
 
-    <button type="submit" class="btn btn-primary">Calculate</button>
+    <button type="submit" class="btn btn-primary" onclick="calculate()">Calculate</button>
+
+    <div style="margin: 15px 0;">
+        <label for="cost">Grading your cards at EGS will cost you: (plus shipping)</label>
+        <input type="text" class="form-control" id="cost" name="cost" readonly>
+    </div>
 
     <p>
         *These numbers are not exact. There can be updates from PSA, higher or lower evaluations of the price or different shipping costs.
@@ -40,7 +45,23 @@
         For more information about PSA and there services visit <a href="https://www.psacard.com/pricing">PSA</a>.
     </p>
 
+    <script type="text/javascript">
+        function calculate(){
+            const economy = document.getElementById("economycard");
+            const regular = document.getElementById("regularcard");
+            const express = document.getElementById("expresscard");
+            const superexpress = document.getElementById("superexpresscard");
+            const walkthrough = document.getElementById("walkthroughcard");
 
+            const cost = document.getElementById("cost");
 
+            let price;
+
+            price = Math.round(economy.value * 50 + regular.value * 100 + express.value * 150 + superexpress.value * 300 + walkthrough.value * 600);
+
+            cost.value = price + ' $';
+
+        }
+    </script>
 
 @endsection
